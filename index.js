f8fe9a7cef5732e0a3cb86bb75fa0ed021023e13
Leone25 <39310565+Leone25@users.noreply.github.com>
2024-12-16 07:01:02 +0100

    fixes

--- index.js
+++ index.js
@@ -29,6 +29,28 @@ const argv = yargs(process.argv)
 		type: 'string',
 		description: 'Output file',
 	})
+	.option('download', {
+		type: 'boolean',
+		description: 'Download the book',
+		default: true,
+		hidden: true,
+	})
+	.option('no-download', {
+		type: 'boolean',
+		description: 'Skip downloading the book and try to extract the zip file that is already in the temp folder',
+		default: false,
+	})
+	.option('clean', {
+		type: 'boolean',
+		description: 'Clean up the temp folder after finishing',
+		default: true,
+		hidden: true,
+	})
+	.option('no-clean', {
+		type: 'boolean',
+		description: 'Don\'t clean up the temp folder after finishing',
+		default: false,
+	})
 	.help()
 	.argv;
 
@@ -45,110 +67,132 @@ function promisify(api) {
 	};
 }
 
-const yauzlFromBuffer = promisify(yauzl.fromBuffer);
+const yauzlFromFile = promisify(yauzl.open);
 
 (async () => {
-	let id = argv.id;
-	let password = argv.password;
-
-	console.log('Warning: this script might log you out of your other devices');
-
-	while (!id)
-		id = prompt('Enter account email: ');
-
-	while (!password)
-		password = prompt('Enter account password: ');
-
-	let userAuth = await fetch('https://npmoffline.sanoma.it/mcs/api/v1/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Timezone-Offset': '+0200', // this is required for whatever reason
-		},
-		body: JSON.stringify({
-			id: id,
-			password: password,
-		}),
-	}).then((res) => res.json()).catch((err) => {
-		console.error('Failed to log in');
-		process.exit(1);
-	});
-
-	if (userAuth.code != 0) {
-		console.error('Failed to log in', userAuth.message);
-		process.exit(1);
-	}
-
-	let gedi = argv.gedi;
-
-	await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/`, {
-		headers: {
-			'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
-		}
-	})
-
-	if (!gedi) {
-		console.log('Fetching book list');
-		let books = {};
-		let pages = 1;
-		for (let i = 1; i <= pages; i++) {
-			let newBooks = await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/books/`, {
-				headers: {
-					'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
-				}
-			}).then((res) => res.json());
-
-			pages = newBooks.result.total_size / newBooks.result.page_size;
-
-			for (let book of newBooks.result.data) {
-				books[book.gedi] = book;
-			}
-		}
-
-		console.log('Books:');
-		console.table(Object.fromEntries(Object.entries(books).map(([id, book]) => [id, book.name])));
-
-		while (!gedi)
-			gedi = prompt('Enter the book\'s gedi: ');
-	}
-
-	console.log('Fetching book data');
-
-	let book = await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/books/${gedi}?app=true`, {
-		headers: {
-			'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
-		}
-	}).then((res) => res.json());
-
-	if (book.code != 0) {
-		console.error('Failed to fetch book data', book.message);
-		process.exit(1);
-	}
-
-	book = book.result.data;
-
-	console.log('Downloading ' + book.name);
-
-	let zip = await fetch(book.url_download).then((res) => res.arrayBuffer());
 
 	await fsExtra.ensureDir('tmp');
 
+	let book;
+
+	if (argv.download) {
+
+		let folder = await fs.promises.readdir('tmp');
+		if (folder.length > 0) {
+			console.log('Temp folder is not empty, make sure to delete the tmp folder if you want to download the book');
+			process.exit(1);
+		}
+
+		let id = argv.id;
+		let password = argv.password;
+
+		console.log('Warning: this script might log you out of your other devices');
+
+		while (!id)
+			id = prompt('Enter account email: ');
+
+		while (!password)
+			password = prompt('Enter account password: ');
+
+		let userAuth = await fetch('https://npmoffline.sanoma.it/mcs/api/v1/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Timezone-Offset': '+0200', // this is required for whatever reason
+			},
+			body: JSON.stringify({
+				id: id,
+				password: password,
+			}),
+		}).then((res) => res.json()).catch((err) => {
+			console.error('Failed to log in');
+			process.exit(1);
+		});
+
+		if (userAuth.code != 0) {
+			console.error('Failed to log in', userAuth.message);
+			process.exit(1);
+		}
+
+		let gedi = argv.gedi;
+
+		await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/`, {
+			headers: {
+				'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
+			}
+		})
+
+		if (!gedi) {
+			console.log('Fetching book list');
+			let books = {};
+			let pages = 1;
+			for (let i = 1; i <= pages; i++) {
+				let newBooks = await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/books/`, {
+					headers: {
+						'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
+					}
+				}).then((res) => res.json());
+
+				pages = newBooks.result.total_size / newBooks.result.page_size;
+
+				for (let book of newBooks.result.data) {
+					books[book.gedi] = book;
+				}
+			}
+
+			console.log('Books:');
+			console.table(Object.fromEntries(Object.entries(books).map(([id, book]) => [id, book.name])));
+
+			while (!gedi)
+				gedi = prompt('Enter the book\'s gedi: ');
+		}
+
+		console.log('Fetching book data');
+
+		book = await fetch(`https://npmoffline.sanoma.it/mcs/users/${id}/products/books/${gedi}?app=true`, {
+			headers: {
+				'X-Auth-Token': 'Bearer ' + userAuth.result.data.access_token,
+			}
+		}).then((res) => res.json());
+
+		if (book.code != 0) {
+			console.error('Failed to fetch book data', book.message);
+			process.exit(1);
+		}
+
+		book = book.result.data;
+
+		console.log('Downloading "' + book.name + '"');
+
+		let zip = await fetch(book.url_download).then((res) => res.arrayBuffer());
+		
+		fs.writeFileSync('tmp/book.zip', Buffer.from(zip));
+	} else {
+		console.log('Skipping download');
+		if (!fs.existsSync('tmp/book.zip')) {
+			console.error('No zip file found in the temp folder');
+			process.exit(1);
+		}
+	}
+
 	console.log('Extracting zip');
 
-	let zipFile = await yauzlFromBuffer(Buffer.from(zip));
+	let zipFile = await yauzlFromFile('tmp/book.zip');
 	let openReadStream = promisify(zipFile.openReadStream.bind(zipFile));
 
 	zipFile.on('entry', async (entry) => {
-		if (!entry.fileName.startsWith("pages") || entry.fileName.endsWith('/')) return;
+		if (!entry.fileName.startsWith("assets/book/pages") || entry.fileName.endsWith('/')) return;
 
-		console.log('Extracting ' + entry.fileName);
+		let filePath = entry.fileName.slice(18);
 
-		let folder = path.dirname(entry.fileName);
-		await fsExtra.ensureDir(`tmp/${folder}`);
+		console.log('Extracting ' + filePath);
+
+		let folder = path.dirname(filePath);
+		await fsExtra.ensureDir(`tmp/pages/${folder}`);
 
 		let page = await openReadStream(entry);
 
-		let file = fs.createWriteStream(`tmp/${entry.fileName}`);
+		let file = fs.createWriteStream(`tmp/pages/${filePath}`);
 		page.pipe(file);
 	});
 
@@ -175,11 +219,22 @@ const yauzlFromBuffer = promisify(yauzl.fromBuffer);
 
 		console.log('Saving PDF');
 
-		await fs.promises.writeFile(argv.output || book.name.replace(/[\\/:*?"<>|]/g, '') + '.pdf', await pdf.save());
+		let name = argv.output;
+		if (argv.download && !name) {
+			name = book.name.replace(/[\\/:*?"<>|]/g, '') + '.pdf';
+		} else if (!name) {
+			name = 'output.pdf';
+		}
 
-		console.log('Cleaning up');
+		await fs.promises.writeFile(name, await pdf.save());
 
-		await fsExtra.remove('tmp');
+		if (argv.clean) {
+			console.log('Cleaning up');
+
+			await fsExtra.remove('tmp');
+		} else {
+			console.log('Skipping clean up, make sure to delete the temp folder when you are done');
+		}
 
 		console.log('Done');
 	});
